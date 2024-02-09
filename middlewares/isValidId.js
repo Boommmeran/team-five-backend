1365e6cd9b1d735d8f0c4e8333b4799db5549509
Oksana Marusich <oksana.m229@gmail.com>
2024-02-09 13:08:13 +0200

    fix bags

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -2,8 +2,8 @@ import { isValidObjectId } from "mongoose";
 import { HttpError } from "../helpers/index.js";
 
 const isValidId = (req, res, next) => {
-  const { contactId } = req.params;
-  if (!isValidObjectId(contactId)) {
+  const { id } = req.params;
+  if (!isValidObjectId(id)) {
     next(HttpError(400, `Invalid search id: ${contactId}`));
   }
   next();
